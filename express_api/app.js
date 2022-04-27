a263f702d42db154219e17fa0a28ef7ed7c3a053
Jesús Álvarez <khisus19@gmail.com>
2022-04-27 08:21:02 -0500

    Paso 3: Creando un endpoint que te regrese un explorer mediando un ID

--- express_api/app.js
+++ express_api/app.js
@@ -7,16 +7,24 @@ app.use(express.json()) // Indicamos que usaremos JSON
 const port = 3000
 
 // HTTP Methods
+// Esto es un end point
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL request ${new Date()}`)
-    const explorer1 = {id: 1, name: "Carlo1"}
-    const explorer2 = {id: 1, name: "Carlo2"}
-    const explorer3 = {id: 1, name: "Carlo3"}
-    const explorer4 = {id: 1, name: "Carlo4"}
+    const explorer1 = {id: 1, name: "Explorer1"}
+    const explorer2 = {id: 2, name: "Explorer2"}
+    const explorer3 = {id: 3, name: "Explorer3"}
+    const explorer4 = {id: 4, name: "Explorer4"}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
+    // HTTP CODE STATUS:
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    
+    const explorer1 = {id: 1, name: "Explorer1"}
+    res.status(200).json(explorer1)
+})
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
